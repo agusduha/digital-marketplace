2dfb6519e2187470536615ab00ce89918daf0e3c
Agustin Duhalde <agustin.duhalde@poincenot.com>
2021-09-29 15:04:05 -0300

    Add mumbai testnet

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const nftmarketaddress = "0x2a0476104B2a0FaCeAB7B5d78248a04B575165AA";
+export const nftaddress = "0x58B7798d06B2f02675f72669b0A82695Fe7fEE65";
